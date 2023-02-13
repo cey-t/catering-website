4f6d3be9824da33613bb38cd72d3ebb865095538
cyd <ceydatoker2209@gmail.com>
2023-02-14 00:53:37 +0200

    update on nav set up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
-import NavBar from "./components/NavBar/NavBar";
+import NavBar from "./components/Layout/NavBar/NavBar";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 
+import styles from "./index.scss";
+import Footer from "./components/Layout/Footer/Footer";
+import Home from "./pages/Home/Home";
+import Features from "./pages/Features/Features";
+import Pricing from "./pages/Pricing/Pricing";
+import About from "./pages/About/About";
+import Premium from "./pages/Premium/Premium";
+import Blog from "./pages/Blog/Blog";
 const App = () => {
-  return <NavBar />;
+  return (
+    <div className={styles.appContainer}>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/premium" element={<Premium />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Blog" element={<Blog />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 };
 export default App;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,19 @@
+import styles from "./Home.module.scss";
+import FeatureContainer from "../../components/FeatureItemContainer/FeatureContainer";
 const Home = () => {
   return (
-    <section>
-      <h1>HOMEPAGE</h1>
-    </section>
+    <>
+      <section className={styles.homePage}>
+        <div className={styles.homeTitleContainer}>
+          <h1 className={styles.homeHeader}>Propel your bussiness</h1>
+          <p className={styles.homeIntro}>
+            Propel comes with everything you need to get your business rolling.
+          </p>
+          <button className={styles.getStartedButton}>Get Started</button>
+        </div>
+      </section>
+      <FeatureContainer />
+    </>
   );
 };
 export default Home;
