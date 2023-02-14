15914a0a55e75f09de3378d8b6d4c64e3d2f2d4f
cyd <ceydatoker2209@gmail.com>
2023-02-14 23:56:16 +0200

    Background - issue fixed

--- src/components/Layout/NavBar/NavBar.js
+++ src/components/Layout/NavBar/NavBar.js
@@ -5,30 +5,28 @@ import styles from "./NavBar.module.scss";
 const NavBar = () => {
   const headerNav = navLinks.slice(0, 6);
   return (
-    <>
-      <section className={styles.headerContainer}>
-        <div className={styles.navBarContainer}>
-          <div className={styles.logoContainer}>
-            <Link to="/" className={styles.logo}>
-              <img src={logo} alt="logo" />
-            </Link>
-          </div>
-          <div className={styles.navLinks}>
-            {headerNav.map((nav) => (
-              <Link to={nav.path} key={nav.id} className={styles.navItem}>
-                {nav.label ? (
-                  nav.label
-                ) : (
-                  <button className={styles.getStartedButton}>
-                    {nav.buttonLabel}
-                  </button>
-                )}
-              </Link>
-            ))}
-          </div>
+    <section className={styles.headerContainer}>
+      <div className={styles.navBarContainer}>
+        <div className={styles.logoContainer}>
+          <Link to="/" className={styles.logo}>
+            <img src={logo} alt="logo" />
+          </Link>
         </div>
-      </section>
-    </>
+        <div className={styles.navLinks}>
+          {headerNav.map((nav) => (
+            <Link to={nav.path} key={nav.id} className={styles.navItem}>
+              {nav.label ? (
+                nav.label
+              ) : (
+                <button className={styles.getStartedButton}>
+                  {nav.buttonLabel}
+                </button>
+              )}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 export default NavBar;
