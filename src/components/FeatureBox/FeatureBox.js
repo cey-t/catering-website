5c69552694cc3481452526d3373fed0928feaedb
cyd <ceydatoker2209@gmail.com>
2023-02-15 23:23:04 +0200

    Feature Item Container Implemented

---
+++ src/components/FeatureBox/FeatureBox.js
@@ -0,0 +1,15 @@
+import styles from "./FeatureContainer.module.scss";
+const FeatureContainer = ({ title, content, logo }) => {
+  return (
+    <div className={styles.featureBox}>
+      <div className={styles.logoContainer}>
+        <img src={logo} alt="logo" />
+      </div>
+      <div className={styles.boxContent}>
+        <h3 className={styles.boxTitle}>{title}</h3>
+        <p className={styles.boxInfo}>{content}</p>
+      </div>
+    </div>
+  );
+};
+export default FeatureContainer;
