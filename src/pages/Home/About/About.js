75cd535bb0447cdd31d64792551e47cc02b5b052
cyd <ceydatoker2209@gmail.com>
2023-02-17 15:28:51 +0200

    Manuel slider implemented

--- src/pages/Home/About/About.js
+++ src/pages/Home/About/About.js
@@ -1,13 +1,49 @@
 import egg from "images/HomePage/egg.jpg";
-// import spagetti from "images/HomePage/spagetti.jpg";
-// import soup from "images/HomePage/soup.jpg";
+import spagetti from "images/HomePage/spagetti.jpg";
+import soup from "images/HomePage/soup.jpg";
+import { useState } from "react";
 import styles from "./About.module.scss";
 import ContextBox from "components/ContextBox/ContextBox";
-
+import classNames from "classnames";
 const About = () => {
+  const Images = [
+    {
+      id: 1,
+      src: egg,
+      alt: "egg",
+    },
+    {
+      id: 2,
+      src: soup,
+      alt: "soup",
+    },
+    { id: 3, src: spagetti, alt: "spagetti" },
+  ];
+  const [counter, setCounter] = useState(1);
+  const [activeDot, setActiveDot] = useState(Images[0].id);
+  const food = Images.filter((num) => num.id === counter)[0];
+  const handleSelectImage = (id) => {
+    setActiveDot(id);
+    setCounter(id);
+  };
   return (
     <section className={styles.aboutSection}>
-      <img src={egg} width={"100%"} alt="soup" />
+      <div className={styles.imageWrapper}>
+        <img src={food.src} width={"100%"} alt={food.alt} />
+
+        <div className={styles.dotContainer}>
+          {Images.map((image) => (
+            <div
+              key={image.id}
+              onClick={() => handleSelectImage(image.id)}
+              className={classNames(styles.sliderDot, {
+                [styles.activeDot]: activeDot === image.id,
+              })}
+            ></div>
+          ))}
+        </div>
+      </div>
+
       <div className={styles.aboutInfo}>
         <ContextBox
           span="Illud decore voluptaria has at."
