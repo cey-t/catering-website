e819ee45888d0473962f8e875a942d281260ce7b
cyd <ceydatoker2209@gmail.com>
2023-02-25 08:54:37 +0200

    features hero implemented

--- src/pages/Premium/Premium.js
+++ src/pages/Premium/Premium.js
@@ -1,4 +1,14 @@
+import HeaderContextBox from "components/HeaderInnerContent/HeaderContextBox";
+import styles from "./Premium.module.scss";
 const Premium = () => {
-  return <h1>Premium</h1>;
+  return (
+    <section className={styles.premiumPage}>
+      <HeaderContextBox
+        name="Premium"
+        header="Personalized and total service."
+        content="At sint tollit veritus duo, ex tota inimicus dissentiunt usu. Dico tamquam perfecto usu id. Eu nec option perfecto vituperatoribus. Ea qui congue patrioque, quo ei recteque consequuntur."
+      />
+    </section>
+  );
 };
 export default Premium;
