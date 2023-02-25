426e09061dc6f42b1eb45d5c1e703927cacbac6b
cyd <ceydatoker2209@gmail.com>
2023-02-25 19:25:53 +0200

    contact form ui

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -1,5 +1,6 @@
 import styles from "./ContactUs.module.scss";
 import HeaderContextBox from "components/HeaderInnerContent/HeaderContextBox";
+import ContactForm from "./ContactForm/ContactForm";
 const ContactUs = () => {
   return (
     <section className={styles.contactUsRoot}>
@@ -8,6 +9,7 @@ const ContactUs = () => {
         header="Have a question for us? Go for it."
         content="At sint tollit veritus duo, ex tota inimicus dissentiunt usu. Dico tamquam perfecto usu id. Eu nec option perfecto vituperatoribus. Ea qui congue patrioque, quo ei recteque consequuntur."
       />
+      <ContactForm />
     </section>
   );
 };
