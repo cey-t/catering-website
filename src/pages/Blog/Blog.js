aac16c1b5b62af06d856d53b4005bd2eeb10fe83
cyd <ceydatoker2209@gmail.com>
2023-03-02 12:47:50 +0200

    blog page set up

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,4 +1,15 @@
+import HeaderContextBox from "components/HeaderInnerContent/HeaderContextBox";
+import ContactUs from "components/ContactSection/ContactUs";
 const Blog = () => {
-  return <h1>Blog</h1>;
+  return (
+    <section>
+      <HeaderContextBox
+        name="Blog"
+        header="Our latest news and events."
+        content="At sint tollit veritus duo, ex tota inimicus dissentiunt usu. Dico tamquam perfecto usu id. Eu nec option perfecto vituperatoribus. Ea qui congue patrioque, quo ei recteque consequuntur."
+      />
+      <ContactUs />
+    </section>
+  );
 };
 export default Blog;
