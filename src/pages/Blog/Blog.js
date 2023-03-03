1ef97711aaaa8f93700a3f6dfc85144d63776982
cyd <ceydatoker2209@gmail.com>
2023-03-03 18:47:33 +0200

    Api Integration

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,6 +1,25 @@
 import HeaderContextBox from "components/HeaderInnerContent/HeaderContextBox";
 import ContactUs from "components/ContactSection/ContactUs";
+import { apiURL } from "utils/api/api";
+import { useEffect, useState } from "react";
+import styles from "./Blog.module.scss";
 const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    const getAllBlogs = async () => {
+      try {
+        const res = await fetch(
+          `${apiURL}/blogs?populate=cardImage,coverImage`
+        );
+        const data = await res.json();
+        if (!res.ok) throw new Error("Something went wrong");
+        setBlogs(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getAllBlogs();
+  }, []);
   return (
     <section>
       <HeaderContextBox
@@ -8,6 +27,16 @@ const Blog = () => {
         header="Our latest news and events."
         content="At sint tollit veritus duo, ex tota inimicus dissentiunt usu. Dico tamquam perfecto usu id. Eu nec option perfecto vituperatoribus. Ea qui congue patrioque, quo ei recteque consequuntur."
       />
+      <div className={styles.blogsContainer}>
+        {blogs?.data?.map(({ attributes, id }) => {
+          console.log(attributes.coverImage.data);
+          const coverImage = attributes.coverImage.data.map(
+            ({ attributes }) => attributes.url
+          );
+
+          return <div>{attributes.content}</div>;
+        })}
+      </div>
       <ContactUs />
     </section>
   );
