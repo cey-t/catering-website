c0ce82129fd72245ea1b5ea3a58937c78a968619
cyd <ceydatoker2209@gmail.com>
2023-03-26 12:19:56 +0300

    Authenticated Forms Refactored - Form component created

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,43 @@
 import styles from "./Form.module.scss";
-const Form = () => {
-  return <form className={styles.formContainer}></form>;
+import { useState } from "react";
+import Input from "./Input/Input";
+import Button from "../Button/Button";
+import { Link } from "react-router-dom";
+const Form = ({ onSubmit, inputs, buttonLabel, link }) => {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(formData);
+  };
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      {inputs.map((input) => (
+        <Input
+          key={input.name}
+          onChange={handleChange}
+          value={formData[input.name] || ""}
+          {...input}
+        />
+      ))}
+      <Button
+        label={buttonLabel}
+        className={styles.button}
+        buttonContainer={styles.buttonContainer}
+      />
+      {link && (
+        <Link to="/signup" className={styles.signupLink}>
+          Sign Up
+        </Link>
+      )}
+    </form>
+  );
 };
 export default Form;
---
+++ src/utils/data/inputData.js
@@ -0,0 +1,23 @@
+export const SignUpInputs = [
+  {
+    type: "text",
+    name: "username",
+    label: "Name",
+    placeholder: "Laura",
+    defaultvalue: "",
+  },
+  {
+    type: "email",
+    name: "email",
+    label: "Email",
+    placeholder: "laura@gmail.com",
+    defaultvalue: "",
+  },
+  {
+    type: "password",
+    name: "password",
+    label: "Password",
+    placeholder: "*********",
+    defaultvalue: "",
+  },
+];
